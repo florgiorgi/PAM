109ddc30fe24e56aff4ab83b826811dc3355e473
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2021-05-02 19:21:18 -0300

    ftu bug fix

--- app/src/main/java/ar/edu/itba/pam/travelapp/FtuActivity.java
+++ app/src/main/java/ar/edu/itba/pam/travelapp/FtuActivity.java
@@ -15,8 +15,8 @@ public class FtuActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ftu);
-        startActivity(new Intent( this, FtuActivity.class));
         Button button = (Button) findViewById(R.id.button_ftu);
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));

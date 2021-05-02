4df90f5cb7517943f541237e0bb162a6652bf690
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2021-05-01 23:59:10 -0300

    FTU Activitiy

--- app/src/main/java/ar/edu/itba/pam/travelapp/MainActivity.java
+++ app/src/main/java/ar/edu/itba/pam/travelapp/MainActivity.java
@@ -2,13 +2,27 @@ package ar.edu.itba.pam.travelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FTU = "ftu";
+    private static final String SP_ID = "travel-buddy-sp";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SharedPreferences sp = getSharedPreferences(SP_ID, MODE_PRIVATE);
+
+        if (sp.getBoolean(FTU, true)) {
+            sp.edit().putBoolean(FTU, false).apply();
+            startActivity(new Intent(this, FtuActivity.class));
+        }
+
     }
 }

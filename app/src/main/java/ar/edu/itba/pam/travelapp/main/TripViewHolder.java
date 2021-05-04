08c3b57da074022478af5469e60b453acec235f1
Unknown <mgiorgi@itba.edu.ar>
2021-05-03 22:24:51 -0300

    all textviews are dynamic in upcoming view

--- app/src/main/java/ar/edu/itba/pam/travelapp/main/TripViewHolder.java
+++ app/src/main/java/ar/edu/itba/pam/travelapp/main/TripViewHolder.java
@@ -9,6 +9,9 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import ar.edu.itba.pam.travelapp.R;
 import ar.edu.itba.pam.travelapp.main.details.DetailsActivity;
 import ar.edu.itba.pam.travelapp.model.trip.Trip;
@@ -32,8 +35,27 @@ public class TripViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bind(final Trip trip) {
-        final TextView textView = itemView.findViewById(R.id.title);
+        final TextView title = itemView.findViewById(R.id.title);
+        final TextView date = itemView.findViewById(R.id.date);
+        final TextView days_left = itemView.findViewById(R.id.expand);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd");
+        String days_left_string = view.getResources().getString(R.string.days_left1);
+
+        String date_from = dateFormat.format(trip.getFrom().getTime());
+        String date_to = dateFormat.format(trip.getTo().getTime());
+
         // TODO: show trip atributes in view
-        textView.setText(trip.getLocation());
+        title.setText(trip.getLocation());
+        date.setText(date_from + " - " + date_to);
+        days_left.setText(calculateDaysDifference(trip.getFrom()) + " " + days_left_string);
+
+    }
+
+    private long calculateDaysDifference(Calendar thatDay) {
+        Calendar today = Calendar.getInstance();
+        long diff = thatDay.getTimeInMillis() - today.getTimeInMillis();
+        long days = diff / (24 * 60 * 60 * 1000);
+
+        return days+1;
     }
 }

23b2305feeb9758e9f4d22d81a072740631920f9
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2021-06-14 00:39:39 -0300

    Error validation

--- app/src/main/java/ar/edu/itba/pam/travelapp/tripdetail/EditTripDialog.java
+++ app/src/main/java/ar/edu/itba/pam/travelapp/tripdetail/EditTripDialog.java
@@ -3,21 +3,18 @@ package ar.edu.itba.pam.travelapp.tripdetail;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-import com.mobsandgeeks.saripaar.Validator;
-import com.mobsandgeeks.saripaar.annotation.Length;
-import com.mobsandgeeks.saripaar.annotation.NotEmpty;
-
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
@@ -38,6 +35,10 @@ public class EditTripDialog extends DialogFragment {
     private Spinner transportSpinner;
     private TravelMethod travelMethod;
 
+    private LocalDate fromDate;
+    private LocalDate toDate;
+    private LocalDateTime departureDate;
+
     private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
@@ -53,16 +54,21 @@ public class EditTripDialog extends DialogFragment {
         builder.setView(view)
                 .setTitle("Edit Trip")
                 .setPositiveButton("Confirm", (dialog, id) -> {
-                    // TODO: create trip and validate errors
-                    listener.editTrip(null);
+                    if (validateInput()) {
+                        Trip trip = new Trip(destination.getText().toString(), fromDate, toDate, travelMethod, departureDate, flightNumber.getText().toString());
+                        listener.editTrip(trip);
+                    }
                 })
                 .setNegativeButton("Cancel", (dialog, id) -> System.out.println("Cancelled"));
 
         from = view.findViewById(R.id.from_edit);
+        from.setOnClickListener(v -> showDateDialog(from));
         to = view.findViewById(R.id.to_edit);
+        to.setOnClickListener(v -> showDateDialog(to));
         destination = view.findViewById(R.id.destination_edit);
         flightNumber = view.findViewById(R.id.flight_number_edit);
         departureTime = view.findViewById(R.id.departure_edit);
+        departureTime.setOnClickListener(v -> showDateTimeDialog(departureTime));
         transportSpinner = view.findViewById(R.id.transport_method_edit);
         transportSpinner.setAdapter(new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, TravelMethod.values()));
         transportSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -76,18 +82,68 @@ public class EditTripDialog extends DialogFragment {
             }
         });
 
-
-
         Trip trip = listener.provideTrip();
         destination.setText(trip.getLocation());
         from.setText(trip.getFrom().format(dateFormatter));
         to.setText(trip.getTo().format(dateFormatter));
         flightNumber.setText(trip.getFlightNumber());
         departureTime.setText(trip.getDepartureTime().format(dateTimeFormatter));
-
         transportSpinner.setSelection(trip.getTravelMethod().ordinal());
 
         return builder.create();
+    }
+
+    private void showDateDialog(EditText inputView) {
+        // TODO
+    }
+
+    public void showDateTimeDialog(EditText inputView) {
+        // TODO
+    }
+
+    private boolean validateInput() {
+        boolean error = false;
+        LocalDate toDate = parseDate(to);
+        LocalDate fromDate = parseDate(from);
+        if (destination.getText() == null || destination.getText().length() == 0) {
+            destination.setError("Field is required");
+            error = true;
+        }
+        if (destination.getText() != null && destination.getText().length() > 20) {
+            destination.setError("Max 20 characters");
+            error = true;
+        }
+        if (toDate == null) {
+            to.setError("Field is required");
+            error = true;
+        }
+        if (fromDate == null) {
+            from.setError("Field is required");
+            error = true;
+        }
+        return !error;
+    }
+
+    private LocalDateTime parseDateTime(EditText inputField) {
+        String dateInput = inputField.getText().toString();
+        LocalDateTime parsedDateTime;
+        try {
+            parsedDateTime = LocalDateTime.parse(dateInput, dateTimeFormatter);
+        } catch (DateTimeParseException e) {
+            parsedDateTime = null;
+        }
+        return parsedDateTime;
+    }
+
+    private LocalDate parseDate(EditText inputField) {
+        String dateInput = inputField.getText().toString();
+        LocalDate parsedDate;
+        try {
+            parsedDate = LocalDate.parse(dateInput, dateFormatter);
+        } catch (DateTimeParseException e) {
+            parsedDate = null;
+        }
+        return parsedDate;
     }
 
     @Override

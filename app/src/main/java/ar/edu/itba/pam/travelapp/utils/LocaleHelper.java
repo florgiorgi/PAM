51af95e07506a8aaf6fd8ff6b6c32b1c9f1f9130
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2021-07-04 20:26:52 -0300

    Change language from app working

---
+++ app/src/main/java/ar/edu/itba/pam/travelapp/utils/LocaleHelper.java
@@ -0,0 +1,75 @@
+package ar.edu.itba.pam.travelapp.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.preference.PreferenceManager;
+
+import java.util.Locale;
+
+public class LocaleHelper {
+
+    private static final String LANGUAGE = "LANGUAGE";
+
+    public static Context onAttach(Context context) {
+        String lang = getPersistedData(context, Locale.getDefault().getLanguage());
+        return setLocale(context, lang);
+    }
+
+    public static Context onAttach(Context context, String defaultLanguage) {
+        String lang = getPersistedData(context, defaultLanguage);
+        return setLocale(context, lang);
+    }
+
+    public static String getLanguage(Context context) {
+        return getPersistedData(context, Locale.getDefault().getLanguage());
+    }
+
+    public static void setLocale(Context c) {
+        setLocale(c, getLanguage(c));
+    }
+
+    public static Context setLocale(Context context, String language) {
+        persist(context, language);
+        return updateResourcesLegacy(context, language);
+    }
+
+    private static String getPersistedData(Context context, String defaultLanguage) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getString(LANGUAGE, defaultLanguage);
+    }
+
+    private static void persist(Context context, String language) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(LANGUAGE, language);
+        editor.apply();
+    }
+
+    private static Context updateResources(Context context, String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Configuration configuration = context.getResources().getConfiguration();
+        configuration.setLocale(locale);
+
+        return context.createConfigurationContext(configuration);
+    }
+
+    private static Context updateResourcesLegacy(Context context, String language) {
+        String country = "US";
+        Locale locale = new Locale(language, country);
+        Locale.setDefault(locale);
+
+        Resources resources = context.getResources();
+        Configuration configuration = resources.getConfiguration();
+        configuration.locale = locale;
+
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+        return context;
+    }
+
+
+}
